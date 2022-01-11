03d44530971cc2384e01793ab94e1478cefc8848
Halo <958708323@qq.com>
2022-01-12 00:11:59 +0800

    添加配置表工具

---
+++ Assets/ConfigTest.cs
@@ -0,0 +1,29 @@
+﻿/* ==============================================================================
+* 功能描述：ConfigTest 
+* 创 建 者：#CreateAuthor#
+* 创建日期：#CREATETIME#
+* ==============================================================================*/
+
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class ConfigTest : MonoBehaviour
+{
+    void Start()
+    {
+        //let data = JsHelpers.ReadAllText(UnityEngine.Application.dataPath + "GameData/" + f + ".json");
+        var tables = new cfg.Tables(file => SimpleJSON.JSON.Parse(
+                File.ReadAllText($"{Application.dataPath}/../Gen/json/{file}.json")
+            )
+        );
+        var item = tables.TbBuff.Get(10110);
+        print($"{item.ID} {item.Name}");
+    }
+
+    void Update()
+    {
+
+    }
+}
