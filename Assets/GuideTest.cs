67f35a6c6c7c6e5b47edb9055943ac66269fc882
Halo <958708323@qq.com>
2022-04-10 19:43:02 +0800

    查找目标规则

---
+++ Assets/GuideTest.cs
@@ -0,0 +1,23 @@
+﻿/* ==============================================================================
+* 功能描述：GuideTest 
+* 创 建 者：#CreateAuthor#
+* 创建日期：#CREATETIME#
+* ==============================================================================*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuideTest : MonoBehaviour
+{
+    void Start()
+    {
+        var list = new List<int> { 1, 2, 3, 4 };
+
+    }
+
+    void Update()
+    {
+        
+    }
+}
